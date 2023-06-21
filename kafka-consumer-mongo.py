cce4ac9d7f140b1152c1787055038cbf3d355dd3
IrvingCM123 <98989957+IrvingCM123@users.noreply.github.com>
2023-06-21 12:02:21 -0600

    cambios

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -5,11 +5,21 @@
 from kafka import KafkaConsumer
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-import os
+
 import json
 
-#uri = "mongodb+srv://alexandrubites:IrvingConde@centeno.naivyqx.mongodb.net/?retryWrites=true&w=majority"
-uri = os.environ['MONGODB_URL']
+uri = "mongodb+srv://alexandrubites:IrvingConde@centeno.naivyqx.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+# client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+
+# try:
+#    client.admin.command('ping')
+#    print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#    print(e)
+
+# Connect to MongoDB and pizza_data database
 
 try:
     client = MongoClient(uri, server_api=ServerApi('1'))
@@ -21,9 +31,9 @@ try:
 except Exception as e:
     print("Could not connect to MongoDB: ", e)
 
-#'my-kafka-0.my-kafka-headless.alexandrubites.svc.cluster.local:9092'
+
 consumer = KafkaConsumer('comments', bootstrap_servers=[
-os.environ['KAFKAURL']])
+'my-kafka-0.my-kafka-headless.alexandrubites.svc.cluster.local:9092'])
 # Parse received data from Kafka
 for msg in consumer:
     record = json.loads(msg.value)
